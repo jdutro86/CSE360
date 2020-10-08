5826cc5b49c2b61a03f4401e3ba2426645b8d4c7
Jason <jdutro@asu.edu>
2020-10-07 19:59:59 -0700

    First Commit

---
+++ Assignment2/src/cse360assignment02/Main.java
@@ -0,0 +1,13 @@
+package cse360assignment02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AddingMachine num = new AddingMachine();
+		num.add(5);
+		num.subtract(3);
+		System.out.println(num.toString());
+	}
+
+}
